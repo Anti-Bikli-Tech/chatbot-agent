4e1f95a102af85ece8c2d9b74533f45f8db8a6cc
anshulgusain <anshulgusain99@gmail.com>
2026-08-19 14:49:46 +0530

    done

--- src/services/instagram.service.js
+++ src/services/instagram.service.js
@@ -13,10 +13,10 @@ async function sendTextMessage(recipientId, text) {
       },
       { headers: { "Content-Type": "application/json" } }
     );
+    console.log("Instagram send success:", res.data);
     return res.data;
   } catch (err) {
-    // console.error("Instagram send error:", err.response?.data || err.message);
-    console.log(JSON.stringify(error.response.data, null, 2));
+    console.error("Instagram send error:", JSON.stringify(err.response?.data || err.message, null, 2));
     throw err;
   }
 }
